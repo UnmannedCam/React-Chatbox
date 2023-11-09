3532a9a5444be3ba5c1e35422d328075f9febbb1
UnmannedCam <80583467+UnmannedCam@users.noreply.github.com>
2023-11-09 15:44:41 +0000

    Initial basic interface design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useRef} from "react";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEVCHATSTACK = [
+    {id: 0, sender: 0, message: 'This is test bubble 1'},
+    {id: 1, sender: 1, message: 'This is test bubble 2'},
+    {id: 2, sender: 1, message: 'This is test bubble 3'},
+    {id: 3, sender: 0, message: 'this is test bubble 4. It\'s quite long. See how long it is? Wow, it really is long.'},
+    {id: 4, sender: 1, message: 'This is yet another bubble, 5'}
+];
+
+const Container = (props) => {
+    return (
+        <div className={'background'}>
+            {props.children}
+        </div>
+    );
+}
+
+
+const ChatContainer = props => {
+    let [chats, setChats] = useState(props.chatStack);
+    const chatScroller = useRef();
+    const scrollToLast = () => {
+        console.log(chatScroller.current)
+        chatScroller.current.scrollIntoView();
+    }
+
+    return (
+        <div className={'chat-container'}>
+            <ChatStack chatStack={chats} ref={chatScroller}/>
+            <MessageSender chats={chats} setChats={setChats} scrollerFunc={scrollToLast}/>
+        </div>
+    )
+}
+
+
+const ChatStack = React.forwardRef((props, ref) => {
+    return (
+        <div className={'chat-stack'}>
+            {props.chatStack.map(chat => <ChatBlock blockInfo={chat} ref={ref} />)}
+        </div>
+    );
+});
+
+
+const MessageSender = props => {
+
+    const setChats = props.setChats;
+    const chats = props.chats;
+    const scrollerFunc = props.scrollerFunc;
+
+    const sendMessage = async inputBox => {
+        if (!inputBox.value) {
+            return
+        }
+        await setChats([...chats, {id: 5, sender: 1, message: inputBox.value}]);
+        inputBox.value = '';
+        scrollerFunc();
+    }
+
+    const _handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage(e.target);
+        }
+    }
+
+    return (
+        <input placeholder={'Type a message...'} className={'chat-input'} onKeyDown={_handleKeyDown} />
+    )
+}
+
+
+const ChatBlock = React.forwardRef((props, ref) => {
+    return (
+        <div className={`chat-block sender-${props.blockInfo.sender}`}>
+            <ChatBubble sender={props.blockInfo.sender} message={props.blockInfo.message} ref={ref}/>
+        </div>
+    );
+});
+
+
+const ChatBubble = React.forwardRef((props, ref) => {
+    return (
+        <div className={`chat-bubble`} ref={ref}>
+            {props.message}
+        </div>
+    );
+});
+
+const App = () => {
+    return (
+        <Container>
+            <ChatContainer chatStack={DEVCHATSTACK} />
+        </Container>
+    );
 }
 
 export default App;
